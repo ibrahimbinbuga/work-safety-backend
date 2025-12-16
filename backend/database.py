9f2326bd8ec8680b6c86062fa1ec91a88843c883
ibrahimbinbuga <ibrahimm3334@icloud.com>
2025-12-16 20:04:07 +0300

    updated database file

--- backend/database.py
+++ backend/database.py
@@ -1,10 +1,25 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql+asyncpg://postgres:derya12sude34@localhost:5432/work_safety_db"
+# 1. .env dosyasını yükle
+load_dotenv()
 
+# 2. Değişkenleri al (Eğer .env yoksa varsayılan olarak ikinci parametreleri kullanır)
+DB_USER = os.getenv("DB_USER", "admin")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "password123")
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = os.getenv("DB_PORT", "5432")
+DB_NAME = os.getenv("DB_NAME", "safety_analysis_db")
+
+# 3. URL'yi oluştur
+DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+# Motoru oluştur
 engine = create_async_engine(DATABASE_URL, echo=True)
 
+# Oturum Oluşturucu
 AsyncSessionLocal = sessionmaker(
     bind=engine,
     class_=AsyncSession,
